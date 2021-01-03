cdbb012e4de001be51641a8c1f19924e1be0e502
gudals113 <61488937+gudals113@users.noreply.github.com>
2021-01-03 22:32:07 +0900

    CRUD 구현

--- tect/app.js
+++ tect/app.js
@@ -38,7 +38,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
 //CONFIGURE ROUTER
 app.use('/subject', subjectRouter)
 app.use('/answer', answerRouter)
@@ -46,111 +45,6 @@ app.use('/question' , questionRouter)
 app.use('/comment', commentRouter)
 app.use('/user', userRouter)
 
-//////////////////////////////////밑으로는 test용 코드들//////////////////////////////
-//////////////////////////////////book&author////////////////////////////////////////
-
-
-const { Author } = require('./models/testModel');
-const { Book } = require('./models/testModel');
-const AUTHOR_OBJECTID = new mongoose.Types.ObjectId;
-const BOOK_OBJECTID = new mongoose.Types.ObjectId;
-
-app.get('/author', (req, res) => {
-  Author.find().populate('books').exec((err, lists) => {
-    if (err) {
-      return res.status(500).send("cannot get author")
-    } else {
-      res.json(lists);
-    }
-  })
-})
-app.post('/author', (req, res) => {
-  const author = new Author();
-  // author._id = AUTHOR_OBJECTID
-  author.name = req.body.name
-  author.age = req.body.age
-
-  //DB에 저장
-  author.save((err) => {
-    if (err) {
-      console.log(err, "data save error");
-      res.json({ result: 0 });
-      return
-    } else {
-      res.json({result:1});
-      // const book1 = new Book({
-      //   title: req.body.title,
-      //   author: author._id
-      // })
-      // book1.save((err) => {
-      //   if (err) {
-      //     res.json({ result: 00 });
-      //   } else {
-      //     res.json({ result: 11 })
-      //   }
-      // })
-    };
-  })
-})
-
-app.get('/book', (req, res) => {
-  Book.find().populate('author').exec((err, lists) => {
-    if (err) {
-      return res.status(500).send("cannot get author")
-    } else {
-      res.json(lists);
-    }
-  })
-});
-app.post('/book', (req, res)=> {
-  const book= new Book();
-  book._id = BOOK_OBJECTID
-  book.title = req.body.title
-  book.book_id =req.body.book_id
-  //DB에 저장
-  book.save((err)=>{
-    if (err) {
-      console.log(err, "data save error");
-      res.json({result: 0});
-      return
-    } else {
-      const author1 = new Author({
-        books : book._id,
-        name : req.body.name,
-        age : req.body.age
-      })
-      author1.save((err)=>{
-        if (err) {
-          res.json({result:00});
-        }
-        else {
-          res.json({result:11})
-        }
-      })
-    }
-  })
-})
-
-app.put('/book/:book_id', (req,res)=>{
-  Book.findOne({book_id:req.params.book_id}, (err, book)=>{
-    if(err) {
-      return res.status(500).json({ error: 'database update failure' });
-    } if(!book) {
-        return res.status(404).json({ error: 'data not found' });
-    } else {
-        if(req.body.book) {
-          
-        }
-        book.save((err)=>{
-            if(err) {
-              res.status(500).json({error: 'failed to update'});
-            } else {
-              res.json({message: 'updated'});
-            }
-        })   
-      }
-  });
-});
 
 
 
