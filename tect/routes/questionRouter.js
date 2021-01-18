b8d4ba537bf44e8eadde179b7221aacf73c23d3c
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-01-18 16:32:32 +0900

    검색쿼리 완료본 통합

--- tect/routes/questionRouter.js
+++ tect/routes/questionRouter.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const router = require('express').Router();
 const mongoose = require('mongoose');
+const async = require( "async" );
 
 const Question = require('../models/question');
 const Answer = require('../models/answer');
@@ -112,37 +113,52 @@ router.post('/search', function(req, res){
   const query=new RegExp(req.body.target);
   var a;
   var uniquearr;
-  Question.find({$or:[{'questionBody.title':query},{'questionBody.content':query}]},'_id',(err,lists)=>{
-    if (err) {
-      return res.status(500).send('Error occurs during serach question')
-    } else {
-      a=lists;
-    }
-  }).exec()
-  .then((Qresult)=>{
-    Answer.find({'answerBody.content':query},'answerBody.postID',(err,lists)=>{
-      if (err) {
-        return res.status(500).send('Error occurs during serach question')
-      } else {
-        a=a.concat(lists);
+  async.waterfall([
+    function(callback){
+      Question.find({$or:[{'questionBody.title':query},{'questionBody.content':query}]},'_id',(err,lists)=>{
+        if (err) {
+          return res.status(500).send('Error occurs during serach question')
+        } else {
+          a=lists;
+          callback(null);
+        }
+      });
+    },
+    function(callback){
+      Answer.find({'answerBody.content':query},{_id:0,'answerBody.postID':1},(err,lists)=>{
+        if (err) {
+          return res.status(500).send('Error occurs during serach question')
+        } else {
+          var result=[];
+          for(var i=0;i<lists.length;i++){
+            var cur={};
+            cur['_id']=lists[i]['answerBody']['postID'];
+            result.push(cur);
+          }
+          a=a.concat(result);
+          // console.log(lists);
+          callback(null);
+        }
+      });
+    },
+    function(callback){
+      const set=new Set(a);
+      uniquearr=[...set];
+      callback(null);
+    }],
+    function(){
+      if(uniquearr.length==0){
+        return res.json([]);
       }
-      }).exec();
-  })
-  .then((QAresult)=>{
-    const set=new Set(a);
-    uniquearr=[...set];
-  })
-  .then((result)=>{
-    if(uniquearr.length==0){
-      return res.json([]);
+      Question.find({$or:uniquearr},(err,lists)=>{
+        if (err) {
+          return res.status(500).send('Error occurs during serach question')
+        } else {
+          res.json(lists);
+        }
+      })
     }
-    Question.find({$or:uniquearr},(err,lists)=>{
-      if (err) {
-        return res.status(500).send('Error occurs during serach question')
-      } else {
-        res.json(lists);
-      }
-    }).exec()
-  })
+  )
 })
+
 module.exports = router;
