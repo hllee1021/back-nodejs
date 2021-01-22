c19baa54d6e683eeb4b54f628f6d6bda3be6603f
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-01-22 14:33:31 +0900

    수정용

--- tect/routes/imageRouter.js
+++ tect/routes/imageRouter.js
@@ -2,14 +2,20 @@ var express = require('express');
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const async = require( "async" );
+const multer=require('multer');
+const upload=multer({dest:'uploads/'})
 
 const Question = require('../models/question');
 const Answer = require('../models/answer');
 const Comment = require('../models/comment');
 const { json } = require('body-parser');
 
-
-
+//글 작성 도중 이미지를 받으면, s3에 저장하고 url 리턴
+router.post('/', upload.single('image'),(req, res) => {
+    console.log(req.file);
+    
+  
+})
 
 
 
