6049a513973ba53d786863b311af96bc2e7f1525
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-03 17:21:47 +0900

    cahce작업중

--- tect/routes/answerRouter.js
+++ tect/routes/answerRouter.js
@@ -9,7 +9,8 @@ const {VERIFY_USER, FIND_MONGO_USER_BY_UID} =require('../firebase/tokenAuth');
 //ANSWER 작성
 router.post('/', async (req, res)=> {
   FIREBASE_USER= await VERIFY_USER(req,res)
-  MONGO_UID = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)[0]._id
+  MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+  MONGO_UID = MONGO_USER[0]._id
   
   const answer= new Answer();
   const POST_ID =req.body.questionID
