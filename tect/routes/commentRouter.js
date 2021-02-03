dfdd2d9cd58ad44dd4c86553cda5200894dee4aa
gudals113 <gudals113@gmail.com>
2021-02-03 16:04:50 +0900

    아

--- tect/routes/commentRouter.js
+++ tect/routes/commentRouter.js
@@ -9,8 +9,8 @@ const {VERIFY_USER, FIND_MONGO_USER_BY_UID} =require('../firebase/tokenAuth');
 //COMMENT 작성
 router.post('/questionComment', async (req, res) => {
   FIREBASE_USER= await VERIFY_USER(req,res)
-  MONGO_UID = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)[0]._id
-
+  MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+  MONGO_UID = MONGO_USER[0]._id
   const comment = new QuestionComment();
   const POST_ID = req.body.questionID
   const PARENT_ID = req.body.parentID
@@ -38,8 +38,9 @@ router.post('/questionComment', async (req, res) => {
 //answerComment 작성 1
 router.post('/answerComment', async (req, res)=>{
   FIREBASE_USER= await VERIFY_USER(req,res)
-  MONGO_UID = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
-
+  MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+  MONGO_UID = MONGO_USER[0]._id
+  
   const comment = new AnswerComment();
   const POST_ID = req.body.answerID
   const PARENT_ID = req.body.parentID
