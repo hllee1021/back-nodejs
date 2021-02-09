0b37bf21fef4767c4595f064c0fe4f773403fe1a
gudals113 <gudals113@gmail.com>
2021-02-09 12:17:55 +0900

    techTree title default

--- tect/models/techTree.js
+++ tect/models/techTree.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 // Define Schemes
 
 const techTreeSchema = new Schema({
-    title:{type:String, required:true},
+    title:{type:String, defalut:''},
     hashtags:[String],
     author:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
     nodeList:{
