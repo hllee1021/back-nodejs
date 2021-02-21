07f48c5978da1422ad4e105e1b38cbda48569cd5
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-21 15:08:50 +0900

    좋아요기능 question에 user 담게 끔 변경작업 시작

--- tect/models/question.js
+++ tect/models/question.js
@@ -12,7 +12,7 @@ const questionSchema = new Schema({
     hashtags:[String],
     like:{type:Number, default:0},
     dislike:{type:Number, default:0},
-
+    like_user:[{type:mongoose.Schema.Types.ObjectId}],
     //V.2021 01 27
     author:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
 
