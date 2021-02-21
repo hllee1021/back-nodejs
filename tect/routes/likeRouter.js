7a42b36187ec9b0c04ea3abb747040940590bb4e
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-21 15:36:10 +0900

    answer, comments, techtree 좋아요 라우터 추가

--- tect/routes/likeRouter.js
+++ tect/routes/likeRouter.js
@@ -6,20 +6,21 @@ const async = require( "async" );
 const Question = require('../models/question');
 const Answer = require('../models/answer');
 const QuestionComment = require('../models/questionComment');
-const AnswerComment = require('../models/answerComment')
+const AnswerComment = require('../models/answerComment');
+const TechTree = require('../models/techTree');
 const { json } = require('body-parser');
 
 const User = require('../models/user')
 const {VERIFY_USER, FIND_MONGO_USER_BY_UID} =require('../firebase/tokenAuth');
 
 
-router.put('/question/like/:id',async(req,res)=>{
+router.put('/question/:id',async(req,res)=>{
     const id=req.params.id;
     // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     FIREBASE_USER= await VERIFY_USER(req,res)
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = MONGO_USER[0]._id
-    const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
+    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
     const question=await Question.findOne({_id:id}).exec();
     // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
@@ -41,6 +42,120 @@ router.put('/question/like/:id',async(req,res)=>{
         }
       )
 })
-  
 
+router.put('/answer/:id',async(req,res)=>{
+    const id=req.params.id;
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    FIREBASE_USER= await VERIFY_USER(req,res)
+    MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+    MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
+    const answer=await Answer.findOne({_id:id}).exec();
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    async.waterfall([
+        function(callback){
+            if(answer.like_user.includes(id)==false){
+                console.log("qwe")
+                Answer.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
+                Answer.findOneAndUpdate({_id:id},{like_user:id}).exec();
+                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+            }
+            else{
+                Answer.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
+                Answer.findOneAndUpdate({_id:id},{$pull:{like_user:id}}).exec();
+            }
+            callback(null);
+        }],
+        function(){
+          res.send("done");
+        }
+      )
+})
+
+router.put('/questioncomments/:id',async(req,res)=>{
+    const id=req.params.id;
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    FIREBASE_USER= await VERIFY_USER(req,res)
+    MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+    MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
+    const questioncomments=await QuestionComment.findOne({_id:id}).exec();
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    async.waterfall([
+        function(callback){
+            if(questioncomments.like_user.includes(id)==false){
+                console.log("qwe")
+                QuestionComment.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
+                QuestionComment.findOneAndUpdate({_id:id},{like_user:id}).exec();
+                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+            }
+            else{
+                QuestionComment.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
+                QuestionComment.findOneAndUpdate({_id:id},{$pull:{like_user:id}}).exec();
+            }
+            callback(null);
+        }],
+        function(){
+          res.send("done");
+        }
+      )
+})
+
+router.put('/answercomments/:id',async(req,res)=>{
+    const id=req.params.id;
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    FIREBASE_USER= await VERIFY_USER(req,res)
+    MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+    MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
+    const answercomments=await AnswerComment.findOne({_id:id}).exec();
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    async.waterfall([
+        function(callback){
+            if(answercomments.like_user.includes(id)==false){
+                console.log("qwe")
+                AnswerComment.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
+                AnswerComment.findOneAndUpdate({_id:id},{like_user:id}).exec();
+                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+            }
+            else{
+                AnswerComment.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
+                AnswerComment.findOneAndUpdate({_id:id},{$pull:{like_user:id}}).exec();
+            }
+            callback(null);
+        }],
+        function(){
+          res.send("done");
+        }
+      )
+})
+
+router.put('/techtree/:id',async(req,res)=>{
+    const id=req.params.id;
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    FIREBASE_USER= await VERIFY_USER(req,res)
+    MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+    MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
+    const techtree=await TechTree.findOne({_id:id}).exec();
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    async.waterfall([
+        function(callback){
+            if(techtree.like_user.includes(id)==false){
+                console.log("qwe")
+                TechTree.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
+                TechTree.findOneAndUpdate({_id:id},{like_user:id}).exec();
+                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+            }
+            else{
+                TechTree.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
+                TechTree.findOneAndUpdate({_id:id},{$pull:{like_user:id}}).exec();
+            }
+            callback(null);
+        }],
+        function(){
+          res.send("done");
+        }
+      )
+})
 module.exports = router;
