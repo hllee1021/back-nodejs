940c66ecf53ffa95007fbf10666cd25c8e4b3b8e
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-21 14:16:53 +0900

    질문추천 기능 firebase 적용

--- tect/routes/likeRouter.js
+++ tect/routes/likeRouter.js
@@ -13,23 +13,23 @@ const User = require('../models/user')
 const {VERIFY_USER, FIND_MONGO_USER_BY_UID} =require('../firebase/tokenAuth');
 
 
-router.get('/question/like/:id',async(req,res)=>{
+router.put('/question/like/:id',async(req,res)=>{
     const id=req.params.id;
-    const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
-    // FIREBASE_USER= await VERIFY_USER(req,res)
-    // MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
-    // MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
+    FIREBASE_USER= await VERIFY_USER(req,res)
+    MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
+    MONGO_UID = MONGO_USER[0]._id
+    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
         function(callback){
-            if(user.like.includes(id)==false){
+            if(MONGO_USER.like.includes(id)==false){
                 console.log("qwe")
                 Question.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
-                User.findOneAndUpdate({_id:'601d1a703118730630b8d2de'},{like:id}).exec();
+                User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
             }
             else{
                 Question.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
-                User.findOneAndUpdate({_id:'601d1a703118730630b8d2de'},{$pull:{like:id}}).exec();
-                user.dislike.splice(user.dislike.indexOf(id),1);
+                User.findOneAndUpdate({_id:MONGO_UID},{$pull:{like:id}}).exec();
             }
             callback(null);
         }],
