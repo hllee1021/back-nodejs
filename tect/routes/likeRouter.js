2b2dd0160a3a3cd283e59af631e0c2b5c2dce30f
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-23 16:24:57 +0900

    좋아요 전체적용

--- tect/routes/likeRouter.js
+++ tect/routes/likeRouter.js
@@ -19,8 +19,6 @@ router.put('/question/:id',async(req,res)=>{
     FIREBASE_USER= await VERIFY_USER(req,res);
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = mongoose.Types.ObjectId(MONGO_USER[0]._id);
-
-    // var user=id;
     const question=await Question.findOne({_id:id}).exec();
     async.waterfall([
         function(callback){
@@ -44,20 +42,16 @@ router.put('/question/:id',async(req,res)=>{
 
 router.put('/answer/:id',async(req,res)=>{
     const id=req.params.id;
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     FIREBASE_USER= await VERIFY_USER(req,res)
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = MONGO_USER[0]._id
-    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
     const answer=await Answer.findOne({_id:id}).exec();
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
         function(callback){
-            if(answer.like_user.includes(id)==false){
+            if(answer.like_user.includes(MONGO_UID)==false){
                 console.log("qwe")
                 Answer.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
-                Answer.findOneAndUpdate({_id:id},{like_user:MONOGO_UID}).exec();
-                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+                Answer.findOneAndUpdate({_id:id},{$push:{like_user:MONGO_UID}}).exec();
             }
             else{
                 Answer.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
@@ -73,19 +67,16 @@ router.put('/answer/:id',async(req,res)=>{
 
 router.put('/questioncomments/:id',async(req,res)=>{
     const id=req.params.id;
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     FIREBASE_USER= await VERIFY_USER(req,res)
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = MONGO_USER[0]._id
-    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
     const questioncomments=await QuestionComment.findOne({_id:id}).exec();
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
         function(callback){
-            if(questioncomments.like_user.includes(id)==false){
+            if(questioncomments.like_user.includes(MONGO_UID)==false){
                 console.log("qwe")
                 QuestionComment.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
-                QuestionComment.findOneAndUpdate({_id:id},{like_user:MONOGO_UID}).exec();
+                QuestionComment.findOneAndUpdate({_id:id},{$push:{like_user:MONGO_UID}}).exec();
                 // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
             }
             else{
@@ -102,20 +93,16 @@ router.put('/questioncomments/:id',async(req,res)=>{
 
 router.put('/answercomments/:id',async(req,res)=>{
     const id=req.params.id;
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     FIREBASE_USER= await VERIFY_USER(req,res)
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = MONGO_USER[0]._id
-    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
     const answercomments=await AnswerComment.findOne({_id:id}).exec();
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
         function(callback){
-            if(answercomments.like_user.includes(id)==false){
+            if(answercomments.like_user.includes(MONGO_UID)==false){
                 console.log("qwe")
                 AnswerComment.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
-                AnswerComment.findOneAndUpdate({_id:id},{like_user:MONOGO_UID}).exec();
-                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+                AnswerComment.findOneAndUpdate({_id:id},{$push:{like_user:MONGO_UID}}).exec();
             }
             else{
                 AnswerComment.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
@@ -131,20 +118,16 @@ router.put('/answercomments/:id',async(req,res)=>{
 
 router.put('/techtree/:id',async(req,res)=>{
     const id=req.params.id;
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     FIREBASE_USER= await VERIFY_USER(req,res)
     MONGO_USER = await FIND_MONGO_USER_BY_UID(FIREBASE_USER.uid)
     MONGO_UID = MONGO_USER[0]._id
-    // const user = await User.findOne({firebaseUid:FIREBASE_USER.uid}).exec();
     const techtree=await TechTree.findOne({_id:id}).exec();
-    // const user = await User.findOne({_id:'601d1a703118730630b8d2de'}).exec();
     async.waterfall([
         function(callback){
-            if(techtree.like_user.includes(id)==false){
+            if(techtree.like_user.includes(MONGO_UID)==false){
                 console.log("qwe")
                 TechTree.findOneAndUpdate({_id:id},{$inc:{like:1}}).exec();
-                TechTree.findOneAndUpdate({_id:id},{like_user:MONOGO_UID}).exec();
-                // User.findOneAndUpdate({_id:MONGO_UID},{like:id}).exec();
+                TechTree.findOneAndUpdate({_id:id},{$push:{like_user:MONGO_UID}}).exec();
             }
             else{
                 TechTree.findOneAndUpdate({_id:id},{$inc:{like:-1}}).exec();
