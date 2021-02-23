f2b68766972562b83c9db06307822e875e82c6b2
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-02-23 15:42:51 +0900

    token 출력시험

--- tect/firebase/tokenAuth.js
+++ tect/firebase/tokenAuth.js
@@ -76,7 +76,8 @@ const VERIFY_USER = async(req, res)=>{
 const FIND_MONGO_USER_BY_UID = async(frontFirebaseUid) =>{
     //aggregate project 이용해서 uid 제외하고 보여주기?
     
-    mongoUser = await User.find({firebaseUid:frontFirebaseUid}).exec()
+    mongoUser = await User.find({firebaseUid:frontFirebaseUid}).exec();
+    console.log(mongoUser);
     console.log(mongoUser[0]._id)
     return mongoUser
 }
